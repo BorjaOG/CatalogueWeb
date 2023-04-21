5696384016305fc709a7eafe2fefa1d7b0109c31
Borja Orge Gesteira <borja_1089@hotmail.com>
2023-04-21 21:46:19 +0800

    error

--- CatalogueWEB/Details.aspx.cs
+++ CatalogueWEB/Details.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Catalogo;
+using Domain;
+using Service;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,60 @@ namespace CatalogueWEB
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                //Inital Config..//
+                if (!IsPostBack)
+                {
+                    MarcaNegocio marcaNegocio = new MarcaNegocio();
+                    List<Marca> listaMarcas = marcaNegocio.listar();
 
+                    CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
+                    List<Categoria> listaCategorias = categoriaNegocio.listar();
+
+                    ddlMarca.DataSource = listaMarcas;
+                    ddlMarca.DataValueField = "Id";
+                    ddlMarca.DataTextField = "Descripcion";
+                    ddlMarca.DataBind();
+
+                    ddlCategoria.DataSource = listaCategorias;
+                    ddlCategoria.DataValueField = "Id";
+                    ddlCategoria.DataTextField = "Descripcion";
+                    ddlCategoria.DataBind();
+                }
+
+                //Modification Config..//
+                string Id = Request.QueryString["Id"] != null ? Request.QueryString["Id"].ToString() : "";
+                if (Id != "" && !IsPostBack)
+                {
+                    ArticuloNegocio negocio = new ArticuloNegocio();                  
+                    Articulo selected = (negocio.listar(Id))[0];
+
+                   
+                    txtId.Text = Id;
+                    txtNombre.Text = selected.Nombre;
+                    txtCodigo.Text = selected.Codigo;
+                    txtDescripcion.Text = selected.Descripcion;
+                    txtUrlImagen.Text = selected.UrlImagen;
+                    txtPrecio.Text = selected.Precio.ToString();
+                    ddlMarca.SelectedValue = selected.Marca.Id.ToString();
+                    ddlMarca.Enabled = false;
+                    ddlCategoria.SelectedValue = selected.Categoria.Id.ToString();
+                    ddlCategoria.Enabled = false;
+
+                    txtUrlImagen_TextChanged(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                Session.Add("error", ex);
+                throw;
+            }
+        }
+
+        protected void txtUrlImagen_TextChanged(object sender, EventArgs e)
+        {
+            imgArticle.ImageUrl = txtUrlImagen.Text;
         }
     }
 }
