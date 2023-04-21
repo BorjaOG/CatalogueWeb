f16d746eb4c7bca671c01263c9874c37a6a52e6b
Borja Orge Gesteira <borja_1089@hotmail.com>
2023-04-21 14:55:14 +0800

    favorites

---
+++ Domain/FAvorite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public int IdUser { get; set; }
+        public int IdArticulo { get; set; }
+
+    }
+}
